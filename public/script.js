d1cca1b9896345e0b3728d5c5b3c6a732bdaaf5d
Adetola Damilola Evaristus <adetolad976@gmail.com>
2026-04-23 02:14:49 +0100

    Initial: Core layout

---
+++ public/script.js
@@ -0,0 +1,12 @@
+/*document.querySelector("main header nav li:first-child");
+
+const deskArray = ["pen", "camera", "phone", "notebook", "headphones", "lightbulb", "USN drive", "elephant"];
+console.log(deskArray);*/
+
+
+
+
+
+
+
+
