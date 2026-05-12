7c55ff165a183eaeb97919298426490ff6a63475
Adetola Damilola Evaristus <adetolad976@gmail.com>
2026-05-12 21:59:33 +0100

    Added Cart page

--- public/script.js
+++ public/script.js
@@ -1,7 +1,3 @@
-/*document.querySelector("main header nav li:first-child");
-
-const deskArray = ["pen", "camera", "phone", "notebook", "headphones", "lightbulb", "USN drive", "elephant"];
-console.log(deskArray);*/
 
 
 
