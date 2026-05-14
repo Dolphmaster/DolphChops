c22dd4e8e597f71fc50fdd02e485da29e0be56ca
Adetola Damilola Evaristus <adetolad976@gmail.com>
2026-05-14 18:12:16 +0100

    Cart Quantity update

--- public/script.js
+++ public/script.js
@@ -1,4 +1,14 @@
+let specialButtons = document.querySelectorAll('#special-dishes button');
+let cartQuantity = document.querySelector('#cart-quantity');
 
+let initialCartQuantity = 0;
+function updateCartQuantity() {
+    initialCartQuantity+= 1;
+    cartQuantity.innerText = initialCartQuantity;
+}
+ specialButtons.forEach(button => {
+        button.addEventListener('click', updateCartQuantity);
+    }); 
 
 
 
